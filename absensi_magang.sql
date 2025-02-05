f53a0ae78d6880ecd7cfb5300e4a75e9813902ed
MuhIlham24 <ilhamofficial24@gmail.com>
2025-02-06 01:22:48 +0800

    Profile dan Ubah Password

--- absensi_magang.sql
+++ absensi_magang.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jan 30, 2025 at 09:28 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: 127.0.0.1
+-- Generation Time: Feb 05, 2025 at 02:57 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -38,7 +38,31 @@ CREATE TABLE `jabatan` (
 
 INSERT INTO `jabatan` (`id`, `jabatan`) VALUES
 (21, 'Admin'),
-(22, 'PTIP');
+(22, 'Mahasiswa Magang'),
+(23, 'IT SUPPORTt');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ketidakhadiran`
+--
+
+CREATE TABLE `ketidakhadiran` (
+  `id` int(11) NOT NULL,
+  `id_mahasiswa` int(11) NOT NULL,
+  `keterangan` varchar(225) NOT NULL,
+  `tanggal` date NOT NULL,
+  `deskripsi` varchar(225) NOT NULL,
+  `file` varchar(225) NOT NULL,
+  `status_pengajuan` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `ketidakhadiran`
+--
+
+INSERT INTO `ketidakhadiran` (`id`, `id_mahasiswa`, `keterangan`, `tanggal`, `deskripsi`, `file`, `status_pengajuan`) VALUES
+(3, 2, 'izin', '2025-02-28', 'lebaran', 'Screenshot 2025-02-04 090817.png', 'APPROVED');
 
 -- --------------------------------------------------------
 
@@ -64,8 +88,7 @@ CREATE TABLE `lokasi_presensi` (
 --
 
 INSERT INTO `lokasi_presensi` (`id`, `nama_lokasi`, `alamat_lokasi`, `tipe_lokasi`, `latitude`, `longitude`, `radius`, `zona_waktu`, `jam_masuk`, `jam_pulang`) VALUES
-(1, 'PN Makassar', 'Jl. Niggacucimotor', 'Pusat', '-5.136298727688664', '119.41358871534342', 100, 'WITA', '07:30:00', '16:30:00'),
-(7, 'rumah', 'Jl. Niggacucimotor', 'Cabang', '-5.143396426308132', '119.4531742264116', 100, 'WITA', '07:30:00', '15:30:00');
+(1, 'PN Makassar', 'Jl. Niggacucimotor', 'Pusat', '-5.136543411784932', '119.41354795067927', 100, 'WITA', '08:00:00', '16:30:00');
 
 -- --------------------------------------------------------
 
@@ -91,10 +114,33 @@ CREATE TABLE `mahasiswa` (
 --
 
 INSERT INTO `mahasiswa` (`id`, `nim`, `nama`, `jenis_kelamin`, `alamat`, `no_handphone`, `divisi`, `universitas`, `lokasi_presensi`, `foto`) VALUES
-(1, '13020220005 ', 'Ilham', 'Laki-laki', 'BTP', '08123456789', 'PTIP', 'UMI', 'Kantor Pengadilan', 'akbar.png'),
-(2, '13020220023', 'Rehan', 'Perempuan', 'Urip', '08912', 'PTIP', 'UMI', 'Kantor Pengadilan', 'akbar.png'),
-(3, 'MHS-0001', 'fikar', 'Laki-Laki', 'jalan sunu', '0943', 'PTIP', 'UMI', 'PN Makassar', 'IMG_0288.jpg'),
-(4, 'MHS-0002', 'nabol ', 'Laki-Laki', 'jalan suni', '094342', 'PTIP', 'UMI', 'PN Makassar', '43509-ilustrasi-marah-freepik (1).jpg');
+(1, 'MHS-0001', 'Tizar', 'Laki-laki', 'BTP', '08123456789', 'Admin', 'UMI', 'PN Makassar', 'akbar.png'),
+(2, 'MHS-0002', 'Rehan', 'Perempuan', 'Urip', '08912', 'Mahasiswa Magang', 'UMI', 'PN Makassar', 'akbar.png'),
+(4, 'MHS-0003', 'Ilham Nur', 'Laki-Laki', 'Jl.baiturrahman', '081342325952', 'IT SUPPORTt', 'umi', 'PN Makassar', '67664d2f0192a.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `presensi`
+--
+
+CREATE TABLE `presensi` (
+  `id` int(11) NOT NULL,
+  `id_mahasiswa` int(11) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `jam_masuk` time NOT NULL,
+  `foto_masuk` varchar(255) NOT NULL,
+  `tanggal_keluar` date NOT NULL,
+  `jam_keluar` time NOT NULL,
+  `foto_keluar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `presensi`
+--
+
+INSERT INTO `presensi` (`id`, `id_mahasiswa`, `tanggal_masuk`, `jam_masuk`, `foto_masuk`, `tanggal_keluar`, `jam_keluar`, `foto_keluar`) VALUES
+(70, 2, '2025-02-05', '13:26:05', 'masuk2025-02-05.png', '2025-02-05', '13:26:10', 'keluar2025-02-05.png');
 
 -- --------------------------------------------------------
 
@@ -105,7 +151,7 @@ INSERT INTO `mahasiswa` (`id`, `nim`, `nama`, `jenis_kelamin`, `alamat`, `no_han
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `id_mahasiswa` int(11) NOT NULL,
-  `username` varchar(225) NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
   `password` varchar(225) NOT NULL,
   `status` varchar(20) NOT NULL,
   `role` varchar(20) NOT NULL
@@ -118,8 +164,7 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `id_mahasiswa`, `username`, `password`, `status`, `role`) VALUES
 (1, 1, 'Tizar', '$2y$10$rjMufkGRZvQLvICHBktYJOFMl9xPYlu5tJyuob8hVfdrUxj09sPT.', 'Aktif', 'admin'),
 (2, 2, 'Rehan', '$2y$10$rjMufkGRZvQLvICHBktYJOFMl9xPYlu5tJyuob8hVfdrUxj09sPT.', 'Aktif', 'mahasiswa'),
-(3, 3, 'fikar', '$2y$10$m8ZanStuQQon4LQHLJWEHuKMRNhSHj6NUFWdH/6dhwc01AZZbQx/a', 'Aktif', 'admin'),
-(4, 4, 'nabil', '$2y$10$dzNR5PHRAx6E1ZdJjmhBBuJxRkZuBcHClPZk59sEco4cBAxGFO27S', 'Aktif', 'admin');
+(5, 4, 'Ilham', '$2y$10$R92BYvZJurJRfzffah3tCOFpElUJ3wFSDzWCBnR74gaGXqzmQVDwC', 'Aktif', 'mahasiswa');
 
 --
 -- Indexes for dumped tables
@@ -132,6 +177,13 @@ ALTER TABLE `jabatan`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `ketidakhadiran`
+--
+ALTER TABLE `ketidakhadiran`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_mahasiswa` (`id_mahasiswa`);
+
+--
 -- Indexes for table `lokasi_presensi`
 --
 ALTER TABLE `lokasi_presensi`
@@ -142,6 +194,13 @@ ALTER TABLE `lokasi_presensi`
 --
 ALTER TABLE `mahasiswa`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `presensi`
+--
+ALTER TABLE `presensi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_mahasiswa` (`id_mahasiswa`);
 
 --
 -- Indexes for table `users`
@@ -159,13 +218,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `jabatan`
 --
 ALTER TABLE `jabatan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `ketidakhadiran`
+--
+ALTER TABLE `ketidakhadiran`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `lokasi_presensi`
 --
 ALTER TABLE `lokasi_presensi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `mahasiswa`
@@ -174,14 +239,32 @@ ALTER TABLE `mahasiswa`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT for table `presensi`
+--
+ALTER TABLE `presensi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `ketidakhadiran`
+--
+ALTER TABLE `ketidakhadiran`
+  ADD CONSTRAINT `ketidakhadiran_ibfk_1` FOREIGN KEY (`id_mahasiswa`) REFERENCES `mahasiswa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `presensi`
+--
+ALTER TABLE `presensi`
+  ADD CONSTRAINT `presensi_ibfk_1` FOREIGN KEY (`id_mahasiswa`) REFERENCES `mahasiswa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `users`
---
+++ admin/fitur_lainnya/profile.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+if (!isset($_SESSION["login"])) {
+    header("Location: ../../auth/login.php?pesan=belum_login");
+} else if ($_SESSION["role"] != 'admin') {
+    header("Location: ../../auth/login.php?pesan=tolak_akses");
+}
+
+$judul = "";
+include('../layout/header.php');
+require_once('../../config.php');
+
+$id = $_SESSION['id'];
+$result = mysqli_query($connection, "SELECT users.id_mahasiswa, users.username, users.status, users.role,
+mahasiswa.* FROM users JOIN mahasiswa ON users.id_mahasiswa = mahasiswa.id WHERE mahasiswa.id = $id");
+?>
+
+<?php while ($mahasiswa = mysqli_fetch_array($result)): ?>
+    <div class="page-body">
+        <div class="container-xl">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="card shadow-lg">
+                        <div class="card-body text-center">
+                            <!-- Profile Picture -->
+                            <img class="rounded-circle mb-4" style="width: 150px; height: 150px; object-fit: cover;"
+                                src="<?= base_url('assets/img/foto_mahasiswa/' . $mahasiswa['foto']) ?>"
+                                alt="Foto Mahasiswa">
+
+                            <!-- Name -->
+                            <h4 class="card-title mb-3"><?= $mahasiswa['nama'] ?></h4>
+
+                            <!-- Details -->
+                            <table class="table table-borderless">
+                                <tbody>
+                                    <tr>
+                                        <th scope="row" class="text-end">Jenis Kelamin:</th>
+                                        <td class="text-start"><?= $mahasiswa['jenis_kelamin'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row" class="text-end">Alamat:</th>
+                                        <td class="text-start"><?= $mahasiswa['alamat'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row" class="text-end">No Handphone:</th>
+                                        <td class="text-start"><?= $mahasiswa['no_handphone'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row" class="text-end">Divisi:</th>
+                                        <td class="text-start"><?= $mahasiswa['divisi'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row" class="text-end">Role:</th>
+                                        <td class="text-start"><?= $mahasiswa['role'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row" class="text-end">Lokasi Presensi:</th>
+                                        <td class="text-start"><?= $mahasiswa['lokasi_presensi'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row" class="text-end">Status:</th>
+                                        <td class="text-start"><?= $mahasiswa['status'] ?></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
+                            <!-- Action Buttons -->
+                            <div class="d-flex justify-content-center gap-2 mt-4">
+                                <a href="<?= base_url('admin/data_mahasiswa/edit.php?id=' . $mahasiswa['id']) ?>"
+                                    class="btn btn-warning">
+                                    <i class="fa-solid fa-pen-to-square"></i> Edit Profil
+                                </a>
+                                <a href="../home/home.php" class="btn btn-secondary">
+                                    <i class="fa-solid fa-arrow-left"></i> Kembali
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php endwhile; ?>
+
+<?php include('../layout/footer.php'); ?>
